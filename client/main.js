d2b5534873441e1c70f9375b1310884ff398e8bc
Moira Leon <moira.leon2@gmail.com>
2021-11-17 20:01:02 -0700

    practicing lists

--- client/main.js
+++ client/main.js
@@ -20,3 +20,17 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+
+Template.body.helpers({
+  tasks: [
+    { text: 'This is task 1' },
+    { text: 'This is task 2' },
+    { text: 'This is task 3' },
+  ],
+  images:[
+    {image: 'https://cdn.petcarerx.com/LPPE/images/articlethumbs/Pomeranian-White-Zoom-In-IG.jpg'},
+    {image: 'https://dogsized.com/wp-content/uploads/2021/08/Fluffy-Dog-Breeds-Pomeranian-.png'},
+    {image: 'https://cdn3.volusion.com/uxmhm.qxgbz/v/vspfiles/photos/RainbowMohawk-1.jpg'},
+    {image: 'https://localist-images.azureedge.net/photos/571993/big_square/fc2e3fd514e0a80e1e980de20c5042493e58170e.jpg'}
+  ]
+});
